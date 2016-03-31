37812075eb7f7da0a1535c7854ccda9994314620
David Eads <davideads@gmail.com>
2016-03-31 14:02:28 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os.path
+from pip.download import PipSession
+from pip.req import parse_requirements
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='copydoc',
+    version='0.0.1',
+    author='David Eads',
+    author_email='deads@npr.org',
+    url='https://github.com/nprapps/copydoc/',
+    description='Parse Google docs for use in content management',
+    long_description=read('README.rst'),
+    py_modules=('copydoc'),
+    license="MIT License",
+    keywords='google gdocs',
+    install_requires=reqs,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    )
+)
